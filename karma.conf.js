19e528aca4341091d11fb754a64d94922c0a079e
Julio Carneiro <jcarneiro@skunkwerks.ca>
2020-03-15 12:32:11 -0300

    upgrade karma config to try to fix Travis CI

--- karma.conf.js
+++ karma.conf.js
@@ -40,7 +40,13 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['Chrome', 'ChromeHeadless', 'ChromeHeadlessNoSandbox'],
+    customLaunchers: {
+      ChromeHeadlessNoSandbox: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox']
+      }
+    },
     crossOriginAttribute: false,
     singleRun: true
   });
